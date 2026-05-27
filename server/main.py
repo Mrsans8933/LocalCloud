b815b1174b69c7dfa2beb4569e369284a0c4ac97
Тимофей <159053603+Mrsans8933@users.noreply.github.com>
2026-05-27 17:12:43 +0300

    Add files via upload

---
+++ server/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def list_files():
+    files = os.listdir('files')
+    html = """
+    <h1>Файлы в облаке</h1><ul>
+    <a href="/upload">загрузить файл</a>
+    """
+    for f in files:
+        html += f'<li><a href="/download/{f}">{f}</a></li>'
+    html += '</ul>'
+    return html
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file.save(f"files/{file.filename}")
+            return "Файл загружен! <a href='/'>Назад</a>"
+    return '''
+        <form method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <button>Загрузить</button>
+        </form>
+    '''
+@app.route("/download/<filename>")
+def download(filename):
+    return send_from_directory(f"files", filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
